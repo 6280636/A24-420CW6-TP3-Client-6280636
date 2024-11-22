422c10f18c815f06386980159f47b4ba4000f4bd
victor <6280636@cegepmontpetit.ca>
2024-11-21 19:27:02 -0500

    FCT Ajustemend du component Score
    Ajustement de la methode changeScoreVisibility et getmyscore

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Score } from '../models/score';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ScoreService } from '../services/score.service';
 
 @Component({
   selector: 'app-score',
@@ -12,18 +15,41 @@ export class ScoreComponent implements OnInit {
   publicScores : Score[] = [];
   userIsConnected : boolean = false;
 
-  constructor() { }
+  constructor(public httpService:HttpClient, public score: ScoreService) { }
 
   async ngOnInit() {
 
     this.userIsConnected = sessionStorage.getItem("token") != null;
-
+    await this.getMyScores();
 
   }
 
   async changeScoreVisibility(score : Score){
 
+    score.isPublic = !score.isPublic
+    await this.score.sendScore(score);
+    if (score.isPublic)
+    {
+      /* await this.score.sendScore(score); */
+    this.publicScores = await lastValueFrom(this.httpService.get<any>("https://localhost:7000/api/Scores/GetScore"));
+    }
+    else
+    {
+      this.publicScores = this.publicScores.filter(s => s.id !== score.id);
+    }
+    //console.log(this.publicScores);
 
   }
+
+  async getMyScores(): Promise<void>{
+    if(this.userIsConnected){
+   
+    this.myScores = await lastValueFrom(this.httpService.get<Score[]>("https://localhost:7000/api/Scores/GetMyScores"));
+   
+    console.log(this.myScores);
+
+    /* return this.publicScores */
+  }
+}
 
 }
